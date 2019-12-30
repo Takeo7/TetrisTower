0cf339d7b26a2f554dce74d8b843f485d1485b18
Arturo Wagener <arthur_wag@hotmail.com>
2019-12-31 00:00:01 +0100

    Core Game

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [Header("Controllers")]
+    [SerializeField]
+    GameController gc;
+
+    [Header("Stats")]
+    [SerializeField]
+    float moveDistance = 1;
+    [SerializeField]
+    bool released = false;
+    [SerializeField]
+    [Range(0.01f,3f)]
+    float fallingVel = 1f;
+    [SerializeField]
+    [Range(0.01f, 3f)]
+    float fallingRefresh = 0.1f;
+    [SerializeField]
+    bool isAcc = false;
+    
+
+    [Header("Components")]
+    [SerializeField]
+    Rigidbody2D rb;
+    [SerializeField]
+    Transform block;
+    [SerializeField]
+    SpriteRenderer sr;
+
+    [Header("Column")]
+    [SerializeField]
+    GameObject column;
+    [SerializeField]
+    [Range(0.1f, 6f)]
+    float sizeColumn = 5f;
+
+    public void SetController(GameController g)
+    {
+        gc = g;
+    }
+
+    IEnumerator Start()
+    {
+        column.transform.localScale = new Vector3(sr.bounds.size.x/sizeColumn, column.transform.localScale.y);
+        while (released.Equals(false))
+        {
+            transform.Translate(Vector3.down * fallingVel);
+            yield return new WaitForSeconds(fallingRefresh);
+        }
+    }
+
+    public void AccelerateFalling()
+    {
+        if (isAcc == false)
+        {
+            StartCoroutine("AccelerateFallingCoroutine");
+        }      
+    }
+
+    IEnumerable AccelerateFallingCoroutine()
+    {
+        fallingRefresh = fallingRefresh * 2;
+        yield return new WaitForSeconds(2f);
+        fallingRefresh = fallingRefresh / 2;
+        isAcc = false;
+    }
+
+    public void Rotate()
+    {
+        block.Rotate(new Vector3(0, 0, -90f));
+        column.transform.localScale = new Vector3(sr.bounds.size.x/sizeColumn, column.transform.localScale.y);
+    }
+
+    public void Move(Direction dir)
+    {
+        switch (dir)
+        {
+            case Direction.Right:
+                transform.Translate(Vector3.right * moveDistance);
+                break;
+            case Direction.Left:
+                transform.Translate(Vector3.left * moveDistance);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Release()
+    {
+        column.SetActive(false);
+        released = true;
+        rb.gravityScale = 1;
+        rb.velocity = Vector3.zero;
+        gc.CurrentBlockRelesaed();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (released == false)
+        {
+            Release();
+        }        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (released == true)
+        {
+            if (collision.CompareTag("KillBlock"))
+            {
+                gc.BlockLost();
+                Destroy(gameObject, 1f);
+            }
+            else if (collision.CompareTag("Finish"))
+            {
+                Debug.Log("Win!!!");
+            }
+        }
+    }
+
+    public enum Direction
+    {
+        Right,
+        Left
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static GameController instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+
+    [Header("PlayerStats")]
+    [SerializeField]
+    byte health = 3;
+
+    [Header("Camera")]
+    [SerializeField]
+    Transform camera;
+    [SerializeField]
+    [Range(0.01f, 3f)]
+    float moveCamera = 0.1f;
+
+    [Header("Blocks")]
+    [SerializeField]
+    GameObject[] blockPrefabs;
+    [SerializeField]
+    Transform spawnPoint;
+    [SerializeField]
+    Block currentBlock;
+
+    [Header("Input Stats")]
+    [SerializeField]
+    Vector3 clickPosition;
+    [SerializeField]
+    float inputDistX;
+    [SerializeField]
+    float inputDistY;
+
+    private void Start()
+    {
+        SpawnNewBlock();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float distX = clickPosition.x - Input.mousePosition.x;
+            
+
+            if (distX <= inputDistX * -1)
+            {
+                currentBlock.Move(Block.Direction.Right);
+                clickPosition = Input.mousePosition;
+            }
+            else if (distX >= inputDistX)
+            {
+                currentBlock.Move(Block.Direction.Left);
+                clickPosition = Input.mousePosition;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            float distY = clickPosition.y - Input.mousePosition.y;
+
+            if (clickPosition.Equals(Input.mousePosition))
+            {
+                currentBlock.Rotate();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickPosition = Input.mousePosition;
+        }
+
+    }
+
+    public void CurrentBlockRelesaed()
+    {
+        SpawnNewBlock();
+    }
+
+
+    void SpawnNewBlock()
+    {
+        int rand = Random.Range(0, blockPrefabs.Length);
+        if (rand.Equals(blockPrefabs.Length))
+        {
+            rand = 0;
+        }
+        currentBlock = Instantiate(blockPrefabs[rand], spawnPoint.position, Quaternion.identity).GetComponent<Block>();
+        currentBlock.SetController(instance);
+
+        camera.Translate(Vector3.up * moveCamera);
+    }
+
+    public void BlockLost()
+    {
+        health--;
+        if (health <= 0)
+        {
+            //GameOver
+            Debug.Log("GameOver");
+        }
+    }
+
+    #region Time Management
+
+    public void PauseTime()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeTime()
+    {
+        Time.timeScale = 1;
+    }
+
+    #endregion
+}
